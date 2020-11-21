8eef1fc9214cf86361d5e30909e09aa82d8f4524
JEland-Git-it-done <57952879+JEland-Git-it-done@users.noreply.github.com>
2020-11-21 20:06:01 +0100

    Added way to read JSON data

--- json_data/tranform_data.py
+++ json_data/tranform_data.py
@@ -1,4 +1,17 @@
 import json
-import pandas
-import numpy
 import os
+
+#This file is used to unspool and transorm the json data into usable sql or json files using my own format
+def clean_files():
+    files = []
+    for file in os.listdir("raw_data"):
+        print(file)
+        files.append(file)
+    for json_file in files:
+        with open("raw_data\{}".format(json_file), "r") as f:
+            print("raw_data/{}".format(json_file))
+            print(f)
+            data = f.read()
+            print(data)
+
+clean_files()
