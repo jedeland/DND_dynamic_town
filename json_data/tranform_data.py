87936eb8649df90fb08ce9438fe3564389fe9959
JEland-Git-it-done <57952879+JEland-Git-it-done@users.noreply.github.com>
2020-11-22 17:03:23 +0100

    Successfully loads python files, issue was backgrounds was somehow empty

--- json_data/tranform_data.py
+++ json_data/tranform_data.py
@@ -1,17 +1,27 @@
 import json
-import os
+import os; import io
+import time
 
 #This file is used to unspool and transorm the json data into usable sql or json files using my own format
+
+
+
 def clean_files():
     files = []
     for file in os.listdir("raw_data"):
         print(file)
         files.append(file)
+
     for json_file in files:
-        with open("raw_data\{}".format(json_file), "r") as f:
-            print("raw_data/{}".format(json_file))
-            print(f)
-            data = f.read()
-            print(data)
+        file_path = 'raw_data/{}'.format(json_file)
+
+        print("raw_data/{}".format(json_file))
+        print("*"*5)
+        f = open(file_path, encoding='utf-8')
+        print("there", f)
+        data = json.load(f)
+        print(data)
+        print(data["backgroundFluff"])
+
 
 clean_files()
