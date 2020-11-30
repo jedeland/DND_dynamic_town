cd09f59c56c34d89279e21551754750975c4482a
JEland-Git-it-done <57952879+JEland-Git-it-done@users.noreply.github.com>
2020-11-30 14:39:27 +0100

    Translated Json Files to Yaml, need to reformat  it

--- json_data/tranform_data.py
+++ json_data/tranform_data.py
@@ -23,11 +23,7 @@ def clean_files():
         f = open(file_path, encoding='utf-8')
         print("IO Wrapper is", f)
         data = json.load(f)
-        #print(data)
-        convert_to_yaml(data, file_path)
-        #print(data)
-        #print("Printing keys - " , data.keys())
-        #Should only load the first key
+        print("Data starts here ... " , data)
         if "fluff" in json_file:
             #Fluff files first object contain all info
             #print(json_file)
@@ -36,14 +32,24 @@ def clean_files():
             #Non fluff files contain metadata in first position, aka 0
             #print("Not fluff file")
             json_key = list(data.keys())[1]
+        try:
+            data.pop("_meta", None)
+            convert_to_yaml(data, file_path)
+        except:
+            pass
+        #print(data)
+        #print(data)
+        #print("Printing keys - " , data.keys())
+        #Should only load the first key
+
         #print("Main Json Key is - ", json_key)
-        simplified_data = data[json_key]
-        print(type(simplified_data))
-        #print(simplified_data)
-        #print("Simplified data stream , ", simplified_data[0])
-        #print("name aquisition ", simplified_data[0]["name"])
+        target_data = data[json_key]
+        print(type(target_data))
+        #print(target_data)
+        #print("Simplified data stream , ", target_data[0])
+        #print("name aquisition ", target_data[0]["name"])
         key_list = []
-        for k in simplified_data:
+        for k in target_data:
             #print("Json part starts here: {} {}".format(json_file, k))
             try:
                 key_list.append(k["name"])
@@ -57,14 +63,27 @@ def clean_files():
     print(json_file_names)
 
 def convert_to_yaml(json_file, old_path):
+    #TODO: Ensure yaml file is not alphabetically loaded
     file_name = old_path.split("/")[1]
     file_name = file_name.split(".")[0]
     #Splits filename at slash and keeps last bit, then splits on . and keeps regular name
     print("File name is : ", file_name)
     new_path = "cleaned_data/{}".format(file_name)
+    print(new_path)
+    with open("{}.yaml".format(new_path), "w+") as f:
+        dump = yaml.dump(json_file, f, default_flow_style=False, sort_keys=False)
+        print("Successfully transformed {} with {}".format(file_name, dump))
+
+def load_yaml():
+
+    for file in os.listdir("cleaned_data"):
+        print(file)
+        with open("cleaned_data/{}".format(file), "r") as out:
+            output = yaml.safe_load(out)
+            print(output)
 
 
 
 
 
-clean_files()
+load_yaml()
