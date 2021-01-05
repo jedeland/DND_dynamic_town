9395f9405d8d243b0b9c59faf02b36687c9edfd4
JEland-Git-it-done <57952879+JEland-Git-it-done@users.noreply.github.com>
2021-01-05 11:41:27 +0100

    Created Store Class

---
+++ json_data/store_class.py
@@ -0,0 +1,8 @@
+
+
+#This file initiates a store class, using data from the above json data and offers options to include pathfinder data
+#Pathfinder data can be found here, with minor changes to the files to make the items 5e compatible
+#https://gitlab.com/jrmiller82/pathfinder-2-sqlite/-/blob/master/data/weapons.yaml
+
+class Store:
+    print("Creating store class")
