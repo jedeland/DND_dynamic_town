717a8c38c3ed495644719315ee76ad5121e20c53
JEland-Git-it-done <57952879+JEland-Git-it-done@users.noreply.github.com>
2021-01-08 16:09:23 +0100

    Checking how to standardise data sets, will do later

--- json_data/transform_store_data.py
+++ json_data/transform_store_data.py
@@ -1,4 +1,6 @@
 import os
+import time
+
 import yaml
 import json
 
@@ -23,8 +25,34 @@ def create_store_combined_file_yaml():
         path_list.append(x)
     print(path_list, "\n",type(path_list))
     path_list.append("cleaned_data/items.yaml")
+    path_list.remove("raw_pathfinder_data/store_data/items-staves.yaml")
     relevant_data = path_list
     print(relevant_data)
+
+    #TODO: go through each file and try to standardise item tags, use this to create two files which hold data about
+    # both types of stores to be used in conjunction with store_class.py
+
+    # for path_item in path_list:
+    #     with open(path_item, "r+") as f:
+    #         store_file = yaml.safe_load(f)
+    #         print(store_file)
+    #         print("\n"*10)
+    #         time.sleep(10)
+    explore_differences(relevant_data)
+
+
+
+def explore_differences(difference_list):
+    with open(difference_list[-1], "r+", encoding="utf-8") as file_1:
+        regular_yaml = yaml.safe_load(file_1)
+    with open(difference_list[-2], "r+", encoding="utf-8") as file_2:
+        json_translated_yaml = yaml.safe_load(file_2)
+    #print(regular_yaml, "\n*\n"*5, json_translated_yaml)
+    print(regular_yaml.keys())
+    regular_yaml = regular_yaml["items"]
+    head = [i for i in regular_yaml][0:10]
+    print(list(enumerate(head)), sep="\n")
+    print(json_translated_yaml.keys())
 
 
 create_store_combined_file_yaml()
