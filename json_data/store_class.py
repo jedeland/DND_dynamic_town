fb0ef00fe3285d25bb7cdc267069a245206cc9a9
JEland-Git-it-done <jedeland@live.co.uk>
2021-03-22 20:45:36 +0100

    Added small skeleton to store_class

--- json_data/store_class.py
+++ json_data/store_class.py
@@ -5,4 +5,17 @@
 #https://gitlab.com/jrmiller82/pathfinder-2-sqlite/-/blob/master/data/weapons.yaml
 
 class Store:
+    def __init__(self, *args):
+        self.name = self.find_valid_name(self.culture)
+        self.store_type = self.determine_store_type(self.remaining_stores)
     print("Creating store class")
+
+    def find_valid_name(self, culture):
+        #This function should use the culture input to determine where the names will come from using method 1 : using name data from the generators before
+        #Method 2 would use a small data scraping database that will reference cultural names : French: boutique, maison ect German: Brauerei, Metzger
+        print("Test name being returned")
+        return "Bobbies Blacksmiths"
+
+    def determine_store_type(self, stores):
+        #This function will use stores (called in world class) to determine which types of stores are still needed, it will take the first call and set up the store
+        return "Blacksmith"
