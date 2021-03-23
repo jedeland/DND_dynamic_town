ccfd2617f1c9c9fd70dd07e2f3ee58ece5137676
JEland-Git-it-done <jedeland@live.co.uk>
2021-03-23 21:55:09 +0100

    Added skeleton files to Store_class.py

--- json_data/store_class.py
+++ json_data/store_class.py
@@ -5,17 +5,20 @@
 #https://gitlab.com/jrmiller82/pathfinder-2-sqlite/-/blob/master/data/weapons.yaml
 
 class Store:
+    #Current args: culture, remaining_stores, region_wealth
     def __init__(self, *args):
         self.name = self.find_valid_name(self.culture)
         self.store_type = self.determine_store_type(self.remaining_stores)
         self.store_wealth = self.determine_regional_wealth(self.region_wealth)
+        self.inventory = self.populate_stock(self.store_wealth)
+        self.notable_npcs = self.create_npcs(self.culture, self.store_wealth)
     print("Creating store class")
 
     def find_valid_name(self, culture):
         #This function should use the culture input to determine where the names will come from using method 1 : using name data from the generators before
         #Method 2 would use a small data scraping database that will reference cultural names : French: boutique, maison ect German: Brauerei, Metzger
         print("Test name being returned")
-        return "Bobbies Blacksmiths"
+        return "Boobies Blacksmiths"
 
     def determine_store_type(self, stores):
         #This function will use stores (called in world class) to determine which types of stores are still needed, it will take the first call and set up the store
@@ -27,7 +30,20 @@ class Store:
         #Populate stock uses the wealth calculation to make a reasonable inventory for the store
         inventory = self.populate_stock(local_wealth, outclasses_area)
         return "Stable"
+
     def populate_stock(self, local_wealth, outclasses_area):
         #Calculates what could be available
         stock = ["Sword", "Dagger", "Horse"]
         print(stock)
+        return stock
+
+    def create_npcs(self, culture, local_wealth):
+        #Creates NPCs using namegen, creates "NPC" Objects
+        print("Npcs")
+        npcs = ["Bob", "Bill", "Jenny"]
+        assign_quests = self.find_quests(npcs, local_wealth)
+        return npcs
+
+    def find_quests(self, npcs, local_wealth):
+
+        return "There are no quests"
