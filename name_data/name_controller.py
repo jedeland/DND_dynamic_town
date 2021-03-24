60a7a613a4685c3c8b2189d734eed448516e7e4c
JEland-Git-it-done <jedeland@live.co.uk>
2021-03-24 21:58:43 +0100

    Added 2 functions to go through new data

--- name_data/name_controller.py
+++ name_data/name_controller.py
@@ -1,5 +1,7 @@
 import json
 import os
+from pprint import pprint
+
 import yaml
 import re
 import sqlite3
@@ -7,4 +9,15 @@ import sqlite3
 def get_names(culture):
     print("Calling the get_names function")
     print(culture)
-    db = sqlite3.connect("names_merged.db")
+    conn = sqlite3.connect("names_merged.db")
+    cur = conn.cursor()
+    #tables = cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    names = cur.execute("""SELECT * FROM NAMES""")
+    pprint(cur.fetchall())
+
+def explore_sql():
+    #This function explores the SQL files
+    sql_list = ["gen_townnames.db", "names_merged.db"]
+    for i in range(2):
+        print("On a fact finding mission! Iteration : {}".format(i))
+get_names("Mexican")
