8c254350e7672a3fa794d7ebec6a8f4df3aec6bb
JEland-Git-it-done <jedeland@live.co.uk>
2021-03-24 22:28:22 +0100

    Added more functionality and made some changes to gen_townnames.db
    changed the column names to lowercase

--- name_data/name_controller.py
+++ name_data/name_controller.py
@@ -11,13 +11,31 @@ def get_names(culture):
     print(culture)
     conn = sqlite3.connect("names_merged.db")
     cur = conn.cursor()
-    #tables = cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    names = cur.execute("""SELECT * FROM NAMES""")
-    pprint(cur.fetchall())
+    cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cur.fetchall()[0][0]
+    #names = cur.execute("""SELECT * FROM NAMES""")
+
+    print(tables)
 
 def explore_sql():
     #This function explores the SQL files
     sql_list = ["gen_townnames.db", "names_merged.db"]
     for i in range(2):
         print("On a fact finding mission! Iteration : {}".format(i))
-get_names("Mexican")
+        facts_dict = {}
+        conn = sqlite3.connect(sql_list[i])
+
+        cur = conn.cursor()
+        cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = cur.fetchall()[0][0]
+        facts_dict["tables"] = tables
+
+        cur.execute("""SELECT sql FROM sqlite_master WHERE tbl_name = '{}' AND type = 'table'""".format(tables))
+        columns = cur.fetchall()
+        print(tables, columns)
+        #Find unique values
+        # if i == 0:
+        #     #Use Name and Origin
+        # else:
+        #     #Use name and origin
+explore_sql()
