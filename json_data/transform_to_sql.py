611a27e25f6521b42468d7bb09f781218fb703cc
JEland-Git-it-done <jedeland@live.co.uk>
2021-04-03 17:05:47 +0200

    uploaded yaml data to pandas df successfully

--- json_data/transform_to_sql.py
+++ json_data/transform_to_sql.py
@@ -3,7 +3,7 @@ import os
 from pprint import pprint
 import yaml
 import sqlite3
-import pandas
+import pandas as pd
 
 def separate_values():
     print("separating values")
@@ -30,6 +30,7 @@ def deconstruct_deities(yaml_file):
     fantasy_deities = yaml_file.copy()
     bad_list = ["greek", "egyptian", "norse", "celtic", "egw", "erlw", "mot", "theros"]
     sources_set = set()
+    deities_dict = []
     for i in yaml_file:
 
         try:
@@ -41,12 +42,22 @@ def deconstruct_deities(yaml_file):
                 #print("\n", i["god_code"], source, "\n")
             else:
                 sources_set.add(i["god_info"]["source"])
+                deities_dict.append(i)
 
         except Exception as e:
             print("Issue is", e, " ", i)
     print("Here are your fantasy versions")
     print(len(fantasy_deities))
+    #deities_json = json.dumps(deities_dict)
+    pd.set_option('display.max_columns', None)
+    new_json = pd.json_normalize(deities_dict, record_prefix=None, meta_prefix=None, max_level=1)
+    new_json.columns = new_json.columns.str.replace('^god_info.', '')
+    new_json = new_json.drop(columns=["altNames", "symbolImg", "_copy", "customExtensionOf"
+        , "reprintAlias", "additionalSources", "srd"])
+    #pprint(deities_dict)
+    print(new_json, type(new_json))
     print(sources_set)
+    #TODO: upload new_json to database table
     #print(fantasy_deities)
 
     #pprint(yaml_file)
@@ -55,5 +66,6 @@ def deconstruct_deities(yaml_file):
 if __name__ == "__main__":
     #Goes over relevant yaml files, adds them to dict using file name
     new_values = separate_values()
+    print("New value is ", type(new_values))
     deconstruct_deities(new_values["deities.yaml"])
     #TODO: add way to check rarity, source and other features to be removed before adding to SQL
