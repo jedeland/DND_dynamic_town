ff2fa1912783c310cfc2eac0ce344256f380d06f
JEland-Git-it-done <jedeland@live.co.uk>
2021-04-17 12:43:22 +0200

    gets ranges from sub dict of inventory_ratios

--- json_data/yaml_controller.py
+++ json_data/yaml_controller.py
@@ -6,5 +6,8 @@ from pprint import  pprint
 
 def assign_categories(yaml_file, inventory_ratios):
     print("Checking over ratios")
-    for i in inventory_ratios:
-        print(i)
+    print(type(inventory_ratios))
+    ranges = inventory_ratios["store_wealth"]
+    print(type(ranges))
+    for k, v in ranges.items():
+        print(k, v)
