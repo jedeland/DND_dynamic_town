4a4e120c88dfb4a13f0a5df2732f1f877d5d0b95
JEland-Git-it-done <jedeland@live.co.uk>
2021-04-19 17:49:33 +0200

    added partial functionality to find_rarity
    Prints set of all rarities listed

--- json_data/yaml_controller.py
+++ json_data/yaml_controller.py
@@ -9,10 +9,24 @@ def assign_categories(yaml_file, inventory_ratios):
     print(type(inventory_ratios))
     ranges = inventory_ratios["store_wealth"]
     print(type(ranges))
+    #TODO have the function add in common items as well
+    ratio_of_items = random.randint(30, 60)
     range_dict = {}
     for k, v in ranges.items():
         print(k, v)
         range_dict[k] = random.choice(v)
     print(range_dict)
 
-def find_rarity
+def find_rarity(yaml_file, inventory_ratios):
+    print()
+    rarity_set = set()
+    print(type(rarity_set))
+    for yaml_dict in yaml_file:
+        try:
+            print()
+            print(yaml_dict["item_info"]["rarity"])
+            if "rarity" in yaml_dict["item_info"].keys():
+                rarity_set.add(yaml_dict["item_info"]["rarity"])
+        except:
+            print("There was an exception at - ", yaml_dict["item_code"])
+            pass
