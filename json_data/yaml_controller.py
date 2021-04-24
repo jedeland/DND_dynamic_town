8807aa7982529af293a7f48e52799ddd044a0337
JEland-Git-it-done <jedeland@live.co.uk>
2021-04-24 15:14:38 +0200

    commented out rarity sets, using rarity bins and inventory ratio to get items randomly

--- json_data/yaml_controller.py
+++ json_data/yaml_controller.py
@@ -19,21 +19,21 @@ def assign_categories(yaml_file, inventory_ratios):
 
 def find_rarity(yaml_file, inventory_ratios):
     print()
-    rarity_set = set()
+    # rarity_set = set()
     # print(type(rarity_set))
-    for yaml_dict in yaml_file:
-        try:
-            # print(yaml_dict["item_info"]["rarity"])
-
-            rarity_set.add(yaml_dict["item_info"]["rarity"])
-        except:
-            #If value is pathfinder item it should be non magical regular item
-            if "CRB" in str(yaml_dict["item_code"]):
-                rarity_set.add("pathfinder_nonmagic")
-            else:
-                print("There was an exception at - ", yaml_dict["item_code"])
-            pass
-    print(rarity_set)
+    # for yaml_dict in yaml_file:
+    #     try:
+    #         # print(yaml_dict["item_info"]["rarity"])
+    #
+    #         rarity_set.add(yaml_dict["item_info"]["rarity"])
+    #     except:
+    #         #If value is pathfinder item it should be non magical regular item
+    #         if "CRB" in str(yaml_dict["item_code"]):
+    #             rarity_set.add("pathfinder_nonmagic")
+    #         else:
+    #             print("There was an exception at - ", yaml_dict["item_code"])
+    #         pass
+    # print(rarity_set)
     print("***")
     rarity_bins = {
         "non-magic": ["pathfinder_nonmagic", "none", "unknown"],
@@ -44,3 +44,11 @@ def find_rarity(yaml_file, inventory_ratios):
         "legendary": ["legendary"],
         "artifact": ["artifact"]
     }
+    print(rarity_bins)
+    inventory_dict = {}
+    for result in yaml_file:
+        print(result["item_info"].keys())
+        if "rarity" in result["item_info"].keys():
+            print("Value is DND item")
+        else:
+            print("Value is pathfinder item")
